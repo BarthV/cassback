9c9d2aeb01eeb9f3080822a48ff56a49638e9b78
Bogdan Niculescu <b.niculescu@criteo.com>
2016-04-26 16:41:55 +0200

    Deployed gem version 0.1.5

--- lib/cassback/version.rb
+++ lib/cassback/version.rb
@@ -1,3 +1,9 @@
 module Cassback
-  VERSION = '0.1.4'.freeze
+  # This represents the version that been currently deployed to rubygems.org
+  # For deploying a new version do the following :
+  # 1) Increment the version here
+  # 2) Build the gem using command : gem build cassback.gemspec
+  # 3) Push the gem using command : gem push cassback-<version>.gem
+  # 4) Commit the version change into source control system.
+  VERSION = '0.1.5'.freeze
 end
