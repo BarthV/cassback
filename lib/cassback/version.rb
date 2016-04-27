2093da3c655b976fca37443805c443687fe63569
Bogdan Niculescu <b.niculescu@criteo.com>
2016-04-27 16:17:34 +0200

    Increased the application version to 0.1.7.

--- lib/cassback/version.rb
+++ lib/cassback/version.rb
@@ -5,5 +5,5 @@ module Cassback
   # 2) Build the gem using command : gem build cassback.gemspec
   # 3) Push the gem using command : gem push cassback-<version>.gem
   # 4) Commit the version change into source control system.
-  VERSION = '0.1.5'.freeze
+  VERSION = '0.1.7'.freeze
 end
